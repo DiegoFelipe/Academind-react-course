654795eac792b1635d0eee055e809bc4a4385361
Diego Felipe <diegofelipe.14@gmail.com>
2021-05-25 19:14:16 -0400

    fixing the firebase url

--- 19 - Advanced Redux/src/App.js
+++ 19 - Advanced Redux/src/App.js
@@ -25,7 +25,7 @@ function App() {
         })
       );
       const response = await fetch(
-        "https://react-http-6b4a6.firebaseio.com/cart.json",
+        "https://react-course-cfb94-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
           body: JSON.stringify(cart),
