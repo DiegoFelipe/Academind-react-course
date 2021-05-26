0f397017ba745c7c000973e0245dce720e84559c
Diego Felipe <diegofelipe.14@gmail.com>
2021-05-25 21:43:55 -0400

    finalizing this topic about advanced redux

--- 19 - Advanced Redux/src/components/store/slices/cartSlice.js
+++ 19 - Advanced Redux/src/components/store/slices/cartSlice.js
@@ -5,30 +5,36 @@ const cartSlice = createSlice({
   initialState: {
     items: [],
     totalQuantity: 0,
+    changed: false,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
-
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
-          name: newItem.title,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
+          name: newItem.title,
         });
       } else {
         existingItem.quantity++;
-        existingItem.totalPrice += newItem.price;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
     removeItemToCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
+      state.changed = true;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
@@ -40,4 +46,5 @@ const cartSlice = createSlice({
 });
 
 export const cartActions = cartSlice.actions;
+
 export default cartSlice;
